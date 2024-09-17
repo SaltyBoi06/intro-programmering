e9786cc269ef9c685d96d704c0cfa6f31d60dbfe
Viktor Svensson <viktor.p.svensson@icloud.com>
2024-09-17 16:10:35 +0200

    e

--- 7040 in o utmatning/uppgift_1.py
+++ 7040 in o utmatning/uppgift_1.py
@@ -1,4 +1,8 @@
-i = 11
-while i > 1:
-    print(i)
-    i = i + -3
+text = input("Hur många heltal: ")
+tal = int(text)
+text = input("Minsta nummer: ")
+tal2 = int(text)
+while tal > 0:
+    print(tal2)
+    tal2 = tal2 + 1
+    tal = tal - 1
---
+++ 7050 while/uppgift1E.py
@@ -0,0 +1,7 @@
+i = 1
+o = 3
+while i < 26:
+    print(i)
+    i = i + o
+    o = o + 2
+print("klar")
